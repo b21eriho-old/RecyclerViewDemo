f8b5789e106125d791891faa3669c7cfe531b4ef
Erik Holmberg <b21eriho@student.his.se>
2022-04-07 09:46:49 +0200

    hehe Data for mountain names, sample data

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView RW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> mountains = Arrays.asList("Kinnekulle", "Billingen", "Mount. Big", "Big-ass Hill", "Skovde mountain");
+
+        RW = findViewById(R.id.recy_view);
+
     }
 }
